d5ae7974950754676914e25e430c703128618b15
DanElis <e1iseev98@mail.ru>
2018-10-03 17:43:43 +0700

    done

--- lab2/client.py
+++ lab2/client.py
@@ -2,8 +2,8 @@ import socket
 import argparse
 import os
 import struct
-
-BUFFER_SIZE = 1024
+from os.path import basename
+BUFFER_SIZE = 4096
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Send file to remote server")
@@ -14,18 +14,20 @@ if __name__ == "__main__":
 
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sock.connect((args.host, args.port))
-
-	with open(args.file, 'rb') as file:
-		sock.send(struct.pack("q", os.path.getsize(args.file)))
-		sock.send(struct.pack('H', len(args.file)))
-		sock.send(bytes(file.name, 'utf8'))
-		data = file.read(BUFFER_SIZE)
-		while len(data) > 0:
-			sock.send(data)
+	try:
+		with open(args.file, 'rb') as file:
+			sock.send(struct.pack("qH", os.path.getsize(args.file),len(bytes(args.file,'utf-8')))+bytes(basename(file.name), 'utf-8'))
 			data = file.read(BUFFER_SIZE)
-	'''data = sock.recv(5)
-	print(data.decode("utf8"))
-	if(data.decode("utf8") == ""):
+			while len(data) > 0:
+				sock.send(data)
+				data = file.read(BUFFER_SIZE)
+		data = sock.recv(1)
+	finally:
+		sock.close()
+
+	info = struct.unpack('!b', data[:1])
+	if(info[0] == 1):
 		print("EXISTS")
 	else:
-		print("NOT EXISTS")'''
+		print("NOT EXISTS")
+
--- lab2/server.py
+++ lab2/server.py
@@ -2,11 +2,21 @@ import socket
 import argparse
 import os
 import struct
+from _thread import start_new_thread
 import threading
+import signal
+import time
+import traceback
 
-BUFFER_SIZE = 1024
+BUFFER_SIZE = 4096
 UPLOAD_DIR = 'uploads/'
-#conn = socket()
+TIMEOUT_SECONDS = 3
+FILE_INFO_SIZE = 10
+INSTANT_SPEED = 0
+AVERAGE_SPEED = 1
+START_TIME = 2
+lock = threading.Lock()
+info_speed = dict()
 def main():
 	parser = argparse.ArgumentParser(description="Receive files from clients")
 	parser.add_argument('port', type=int, help='Port to listen to', metavar='Port')
@@ -15,36 +25,78 @@ def main():
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sock.bind(('', args.port))
 	sock.listen(10)
-	while (True):
-		conn, addr = sock.accept()
-		print(type(conn))
-		print('Connection address: {0}'.format(addr))
-		#threading.Thread(target=my_tcp_server).start()
-		my_tcp_server(conn)
-		break
 
-def my_tcp_server(conn):
-	data = conn.recv(BUFFER_SIZE)
-	conn.setblocking(0)
-	conn.settimeout(2)
-	size_file,filename_size = struct.unpack('qH', data[:10])
-	
-	filename = str(data[10:], "utf8")
-	file = open(UPLOAD_DIR + filename[:filename_size], 'wb+')
-	if(len(filename) == filename_size):
-		data = conn.recv(BUFFER_SIZE)
-	else:
-		data = bytes(filename[filename_size:],"utf8")
-	while(size_file > 0):
-		size_file -= BUFFER_SIZE
-		file.write(data)
-		try:
+	try:	
+		signal.signal(signal.SIGALRM, handle_timeout)
+		signal.alarm(TIMEOUT_SECONDS)
+		while(True):
+			conn, addr = sock.accept()
+			print('Connection address: {0}'.format(addr))
+			start_new_thread(client_thread, (conn,))
+			
+	except KeyboardInterrupt:
+		print("")
+	finally:
+		sock.close()
+		conn.close()
+
+def client_thread(conn):
+	conn.settimeout(60)
+	try:
+		init_speed(conn)
+		data = read_file_info(conn)
+		size_file,filename_size = struct.unpack('qH', data[:10])
+		filename = read_filename(filename_size,conn)
+		file = open(UPLOAD_DIR + filename[:filename_size], 'wb+')
+			
+		while (file.tell() < size_file):
 			data = conn.recv(BUFFER_SIZE)
-		except socket.timeout:
-			continue
-	file.close()
-	conn.send(struct.pack('!b', 0))
-	conn.close()
+			update_speed(key = conn,length_data = len(data))
+			if not data:
+				break
+			file.write(data)
+		conn.send(struct.pack('!b', 1))
+	except BaseException:
+		conn.send(struct.pack('!b', 2))
+	finally:
+		delete_speed(conn)
+		file.close()
+		conn.close()
+def init_speed(key):
+	lock.acquire()
+	info_speed[key] = [0,0,time.time()]
+	lock.release()
+def delete_speed(key):
+	lock.acquire()
+	del(info_speed[key])
+	lock.release()
+def read_file_info(conn):
+	data = conn.recv(FILE_INFO_SIZE)
+	while(len(data) != 10):
+		data += conn.recv(FILE_INFO_SIZE - len(data))
+	return data
+def update_speed(key,length_data):
+	lock.acquire()
+	info_speed[key][INSTANT_SPEED] += length_data
+	info_speed[key][AVERAGE_SPEED] += length_data
+	lock.release()
+def handle_timeout(signum,frame):
+	lock.acquire()
+	for key in info_speed:
+		print(key)
+		print("Instant speed bytes/s")
+		print(info_speed[key][INSTANT_SPEED]/TIMEOUT_SECONDS)
+		print("Average speed bytes/s")
+		print(info_speed[key][AVERAGE_SPEED]/(time.time()-info_speed[key][START_TIME]))
+		info_speed[key][INSTANT_SPEED] = 0
+	lock.release()
+	signal.alarm(TIMEOUT_SECONDS)
+def read_filename(filename_size,conn):
+	filename = conn.recv(filename_size)
+	while(len(filename) != filename_size):
+		filename += conn.recv(filename_size-len(filename))
+
+	return str(filename[:filename_size], "utf-8")
 
 if __name__ == "__main__":
 	main()
